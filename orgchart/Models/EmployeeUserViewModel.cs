130cf697c7d02e075f8d4bd55e51f96ee76f967a
sadiredwan <sadi.redwan@gmail.com>
2019-05-30 13:14:50 +0600

    Views/EmployeeUser/assign.cshtml Updated.

--- orgchart/Models/EmployeeUserViewModel.cs
+++ orgchart/Models/EmployeeUserViewModel.cs
@@ -11,6 +11,10 @@ namespace orgchart.Models
         public User VM_USER { get; set; }
         public List<Employee> VM_EMPLOYEES { get; set; }
         public Employee VM_EMPLOYEE { get; set; }
+        public List<Designation> VM_DESIGNATIONS { get; set; }
+        public Designation VM_DESIGNATION { get; set; }
+        public List<Department> VM_DEPARTMENTS { get; set; }
+        public Department VM_DEPARTMENT { get; set; }
 
         public List<EmployeeUser> VM_EMPLOYEEUSERS { get; set; }
         public EmployeeUser VM_EMPLOYEEUSER { get; set; }
